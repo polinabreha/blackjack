d63ccdc698e9b8addc11681bc17ae78647aa3026
polinabreha <polinabreha@gmail.com>
2026-05-07 12:51:37 -0400

    added classes

--- src/main/java/com/pluralsight/MainApp.java
+++ src/main/java/com/pluralsight/MainApp.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
 public class MainApp {
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        Hand hand1 = new Hand();
+
+        for(int i = 0; i < 5; i++) {
+            Card card = deck.deal();
+            hand1.deal(card);
+        }
+        int handValue = hand1.getValue();
+        System.out.println("This hand is worth " + handValue);
+    }
 }
